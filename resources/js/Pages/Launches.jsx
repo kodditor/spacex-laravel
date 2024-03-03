8c840feefd5e9f7cd21bb6448ba4affe551618cd
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-03 12:21:33 +0000

    Initial commit

---
+++ resources/js/Pages/Launches.jsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react'
+
+export default function Launches(){
+
+    return (
+        <>
+            <Head title='SpaceX Launches' />
+            <body>
+                <p>Hello World!</p>
+            </body>
+        </>
+    )
+}
